1d2884813619a0b3afaa0750aa1e8fdc318f5e8b
lai2-24 <lai2-24@itismeucci.com>
2024-10-23 09:58:19 +0200

    primo

---
+++ client/src/main/java/com/example/Main.java
@@ -0,0 +1,42 @@
+package com.example;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws UnknownHostException, IOException{
+        Socket s = new Socket("localhost",3000);
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        DataOutputStream out = new DataOutputStream(s.getOutputStream());
+
+        String n = "";
+        String ris = "";
+        String tentativi = "";
+
+        Scanner sc  = new Scanner(System.in);
+        do{
+            System.out.println("Inserire numero");
+                n = sc.nextLine();
+                out.writeBytes(n + "\n");
+                ris=in.readLine();
+                
+                if(ris.equals("<"))
+                    System.out.println("Numero troppo piccolo");
+                else if(ris.equals(">"))
+                    System.out.println("Numero troppo grande");
+                else{
+                tentativi = in.readLine();
+                System.out.println("HAI INDOVINATO in " + tentativi + " tentativi.");
+                }
+            
+            
+        }while(!ris.equals("="));
+        s.close();
+    }
+}
---
+++ server/src/main/java/com/example/Controllo.java
@@ -0,0 +1,53 @@
+package com.example;
+
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.Random;
+
+public class Controllo extends Thread{
+    Socket s;
+
+    Controllo(Socket s){
+        this.s = s;
+    }
+    
+    public void run()
+    {
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            DataOutputStream out = new DataOutputStream(s.getOutputStream());
+            String stringNum = "";
+            int intNum = 0;
+            Random random = new Random();
+            int numero = random.nextInt(100);
+            System.out.println(numero);
+            int tent = 0; 
+            boolean ind = false;
+
+            do{
+                tent++;
+                stringNum = in.readLine();
+                intNum = Integer.parseInt(stringNum);
+                if(intNum == numero)
+                {
+                    ind = true;
+                    out.writeBytes("=" + "\n" );
+                    out.writeBytes(tent + "\n" );
+                }
+                  
+                else if(intNum > numero)
+                    out.writeBytes(">" + "\n" );
+                else if(intNum < numero)
+                    out.writeBytes("<" + "\n");
+            }while(!ind);
+
+        }catch (IOException e){ 
+            e.printStackTrace();
+        }
+        System.out.println("Client disconnesso");
+    }
+}

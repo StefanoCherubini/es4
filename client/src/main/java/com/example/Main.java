331b38274ce3f3a0231e57549d73c06356f433d9
StefanoCherubini <cherubini.stefano@itismeucci.com>
2024-10-28 19:41:32 +0100

    aggiunta piu di una partita

--- client/src/main/java/com/example/Main.java
+++ client/src/main/java/com/example/Main.java
@@ -10,37 +10,50 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws UnknownHostException, IOException{
-        Socket s = new Socket("localhost",3000);
+        Scanner sc = new Scanner(System.in);
+        boolean playAgain;
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-        DataOutputStream out = new DataOutputStream(s.getOutputStream());
+        do {
+            // Avvio della connessione per una nuova partita
+            Socket s = new Socket("localhost", 3000);
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            DataOutputStream out = new DataOutputStream(s.getOutputStream());
 
-        String n = "";
-        String ris = "";
-        String tentativi = "";
+            String n = "";
+            String ris = "";
+            String tentativi = "";
 
-        Scanner sc  = new Scanner(System.in);
-        do{
-            System.out.println("Inserire numero");
+
+            do {
+                System.out.println("Inserire numero");
                 n = sc.nextLine();
                 out.writeBytes(n + "\n");
-                ris=in.readLine();
-                
-                if(ris.equals("!"))
-                {
-                System.out.println("Inserire un numero da 0 a 100");
-                }
-                else if(ris.equals("<"))
+                ris = in.readLine();
+
+                if (ris.equals("!")) {
+                    System.out.println("Inserire un numero da 0 a 100");
+                } else if (ris.equals("<")) {
                     System.out.println("Numero troppo piccolo");
-                else if(ris.equals(">"))
+                } else if (ris.equals(">")) {
                     System.out.println("Numero troppo grande");
-                else{
-                tentativi = in.readLine();
-                System.out.println("HAI INDOVINATO in " + tentativi + " tentativi.");
+                } else {
+                    tentativi = in.readLine();
+                    System.out.println("HAI INDOVINATO in " + tentativi + " tentativi.");
                 }
-            
-            
-        }while(!ris.equals("="));
-        s.close();
+
+            } while (!ris.equals("="));
+
+            // Chiusura della connessione per la partita corrente
+            s.close();
+
+            // Chiedere se si vuole giocare di nuovo
+            System.out.println("Vuoi giocare un'altra partita? (s/n)");
+            String response = sc.nextLine().trim().toLowerCase();
+            playAgain = response.equals("s");
+
+        } while (playAgain);
+
+        System.out.println("Grazie per aver giocato!");
+        sc.close();
     }
 }
